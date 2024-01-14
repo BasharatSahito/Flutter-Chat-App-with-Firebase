fedff86d28e10632bff5bd68ea33210b7630141c
BasharatSahito <basharataliali3@gmail.com>
2024-01-14 09:08:56 +0500

    Added .env file with sensitive api keys and added it to the git ignore and also added a .env_sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,7 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/services/apis.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 class CallPage extends StatelessWidget {
@@ -10,10 +11,10 @@ class CallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint("Call ID $callID");
     return ZegoUIKitPrebuiltCall(
-        appID:
-            645377989, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
-        appSign:
-            "272c7548fc8c136fdb14d9ac500c7bee75951a11303de09d467afeb4c8891738", // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+        appID: int.tryParse(dotenv.env['ZEGO_APP_ID'] ?? '') ??
+            0, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+        appSign: dotenv.env['ZEGO_APP_SIGN'] ??
+            '', // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
         userID: APIs.auth.currentUser!.uid,
         userName: APIs.auth.currentUser!.displayName.toString(),
         callID: callID,
